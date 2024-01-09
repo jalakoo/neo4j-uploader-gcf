f6d182974d40cfc4bb47e8c7340ed31b809114f4
Jason Koo <jalakoo@gmail.com>
2024-01-08 17:57:08 -0800

    Simplified code using uploader error handling and using neo4j credentials from env variables

--- main.py
+++ main.py
@@ -1,27 +1,20 @@
 import functions_framework
-from neo4j_uploader import batch_upload
+from neo4j_uploader import batch_upload, InvalidPayloadError
 import os
 
 @functions_framework.http
 def json_to_neo4j(request):
 
-    # Make sure we can extract the JSON payload from the request
-
-    # content_type = request.headers['content-type']
-    # if content_type != 'application/json':
-    #     return "Content-type must be type application/json", 400
-    
+    # Validate a JSON payload is available
     try:
         request_json = request.get_json(silent=True)
     except Exception as e:
-        return f"Exception getting JSON: {e}", 400
-    
+        return f"JSON payload missing or malformed: {e}", 400
+
     # Validate config information is available
     uri = os.environ.get('NEO4J_URI', None)
     user = os.environ.get('NEO4J_USERNAME', None)
     password = os.environ.get('NEO4J_PASSWORD', None)
-    if uri is None or user is None or password is None:
-        return 'Neo4j credentials missing', 500
     
     # Forward the request to the neo4j-uploader
     try:
@@ -31,8 +24,12 @@ def json_to_neo4j(request):
                 'neo4j_user': user,
                 'neo4j_password': password
             },
-            data = request_json
+            data = request_json,
             )
         return upload_result.model_dump(), 200, {"Content-Type": "application/json"}
+    except InvalidPayloadError as e:
+        # Missing or Invalid JSON payload
+        return f'{e}', 401
     except Exception as e:
+        # Other neo4j exception or uploader error
         return f'Problem uploading: {e}', 500
