645b8591dbb982fe13e3c367bb7f7935a0a713a3
jalakoo <jalakoo@users.noreply.github.com>
2024-01-08 17:59:12 -0800

    0.2.0 (#2)
    * Reset option added
    
    * Simplified code using uploader error handling
    
    * neo4j credentials from .json payload removed - pulls from env variables instead

--- main.py
+++ main.py
@@ -1,91 +1,35 @@
 import functions_framework
-from pydantic import BaseModel, validator, parse_obj_as
-from neo4j_uploader import upload, start_logging, clear_db
-import logging
-import json
-import asyncio
-
-logging.getLogger().setLevel(logging.DEBUG)
-logging.info(f'App Started')
-
-class CreateDynamicRequest(BaseModel):
-    neo4j_uri: str
-    neo4j_user: str
-    neo4j_password: str
-    data: dict
-    node_key :str = "_uid"
-    dedupe: bool = False
-    # @validator("neo4j_uri", "neo4j_user", "neo4j_password", pre=True, always=True)
-    # def convert_to_none(cls, value):
-    #     if isinstance(value, str) and value.strip() == "":
-    #         return None
-    #     return value
+from neo4j_uploader import batch_upload, InvalidPayloadError
+import os
 
 @functions_framework.http
-def gcf_upload(request):
-    content_type = request.headers['content-type']
-    if content_type != 'application/json':
-        return "Content-type must be type application/json"
-    
+def json_to_neo4j(request):
+
+    # Validate a JSON payload is available
     try:
         request_json = request.get_json(silent=True)
     except Exception as e:
-        print(f'Exception getting JSON: {e}')
-        return "Exception getting JSON", 400
+        return f"JSON payload missing or malformed: {e}", 400
+
+    # Validate config information is available
+    uri = os.environ.get('NEO4J_URI', None)
+    user = os.environ.get('NEO4J_USERNAME', None)
+    password = os.environ.get('NEO4J_PASSWORD', None)
     
-    print(f'received json: {request_json}')
-    start_logging()
-
-    # Why isn't Pydantic working here?
-    # nRequest = CreateDynamicRequest.model_validate(request_json)
-    
-    uri = request_json.get('neo4j_uri')
-    user = request_json.get('neo4j_user')
-    password = request_json.get('neo4j_password')
-    node_key = request_json.get('node_key', "_uid")
-    dedupe = request_json.get('dedupe', False)
-    data = request_json.get('data')
-    batch_size = request_json.get('batch_size', 500)
-    overwrite = request_json.get('overwrite', False)
-
-    print(f'uri: {uri}, user: {user}, pass: {password}, node_key:{node_key}, data:{data}')
-
+    # Forward the request to the neo4j-uploader
     try:
-        time, nodes_created, rel_created, props_set = upload(
-            (uri, user, password), 
-            data, 
-            node_key=node_key, 
-            dedupe_nodes=dedupe, 
-            dedupe_relationships = dedupe,
-            max_batch_size = batch_size,
-            should_overwrite = overwrite
+        upload_result = batch_upload(
+            config = {
+                'neo4j_uri': uri,
+                'neo4j_user': user,
+                'neo4j_password': password
+            },
+            data = request_json,
             )
-        return json.dumps({'seconds_to_complete': time, 'nodes_created': nodes_created,'rel_created': rel_created, 'props_set': props_set}), 200, {"Content-Type": "application/json"}
+        return upload_result.model_dump(), 200, {"Content-Type": "application/json"}
+    except InvalidPayloadError as e:
+        # Missing or Invalid JSON payload
+        return f'{e}', 401
     except Exception as e:
+        # Other neo4j exception or uploader error
         return f'Problem uploading: {e}', 500
-
-@functions_framework.http
-def gcf_reset(request):
-    content_type = request.headers['content-type']
-    if content_type != 'application/json':
-        return "Content-type must be type application/json"
-    
-    try:
-        request_json = request.get_json(silent=True)
-    except Exception as e:
-        print(f'Exception getting JSON: {e}')
-        return "Exception getting JSON", 400
-    
-    print(f'received json: {request_json}')
-    start_logging()
-    
-    uri = request_json.get('neo4j_uri')
-    user = request_json.get('neo4j_user')
-    password = request_json.get('neo4j_password')
-    database = request_json.get('neo4j_database', "neo4j")
-
-    result = clear_db(
-        creds = (uri, user, password),
-        database = database
-    )
-    return f'Result: {result}', 200
